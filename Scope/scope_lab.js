80a6d376af7786bf8acf2b97ed3e5547519bfdc5
Pattiecodes <deocampojamespatrickcode@gmail.com>
2024-09-09 22:22:00 -0400

    added new files

---
+++ Scope/scope_lab.js
@@ -0,0 +1,32 @@
+//Global Scope
+var globalVar = "I'm a global variable";
+let globalLet = "I'm also global, but scoped with let";
+const globalConst = "I'm a global constant";
+
+{
+    //Block Scope
+    var blockVar = "I'm a block-scoped var";
+    let blockLet = "I'm a block-scoped let";
+    const blockConst = "I'm a block-scoped const";
+}
+
+// Global scope
+console.log(globalVar); // Output: "I'm a global variable"
+console.log(globalLet); // Output: "I'm also global, but scoped with let"
+console.log(globalConst); // Output: "I'm a global constant"
+
+//Block Scope //Commented the below code:
+//console.log(blockVar);
+//console.log(blockLet);
+
+//added function show()
+function show() {
+    var functionVar = "I'm a block-scoped var";
+    let functionLet = "I'm a block-scoped let";
+    const functionCost = "I'm a block-scoped const";
+}
+show();
+
+console.log(functionVar); //Throws ReferenceError
+console.log(functionLet); //Throws ReferenceError
+console.log(functionCost); //Throws ReferenceError
